b7a14888506bffaef53e516e454692f452fbe613
thunderrealm <38511635+thunderrealm@users.noreply.github.com>
2018-09-20 17:25:18 +0100

    Added 'get' fucntions
    Added the get phones get emeails and getlocs fucntions to scrape the data from the page in question, which has now been made a variable.
    
    Co-Authored-By: NeonDrive <neondrive@users.noreply.github.com>

--- Folder/TestApp.js
+++ Folder/TestApp.js
@@ -10,26 +10,25 @@ var knwlInstance = new knwl('English');
 
 // load https
 const https = require('https');
+const http = require('http');
 
-var msg = "The modules are loaded";
+var msg = "\n The modules are loaded \n";
 console.log(msg);
 
+// create a container to store teh main info we want in
+var phoneList = [];
+var emailList = [];
+var locList = [];
+
 // copied from https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
-// seems to get the description of the astronomy picture of the day
-// commented out so i have a copy that i can refer back to
-// https.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY', (resp) =>
-// {
-//   let data = '';
 
-//   // A chunk of data has been recieved.
-//   resp.on('data', (chunk) => {data += chunk;});
+// TODO: check if its http or https for each site
+// TODO: put into a function that takes a URL string as a parameter
+var URLin = 'https://www.canddi.com';
 
-//   // The whole response has been received. Print out the result.
-//   resp.on('end', () => {console.log(JSON.parse(data).explanation);});
+console.log("\n Searching " + URLin + " for data.")
 
-// }).on("error", (err) => {console.log("Error: " + err.message);});
-
-https.get('https://www.canddi.com', (resp) =>
+https.get(URLin, (resp) =>
 {
   let data = '';
 
@@ -37,7 +36,7 @@ https.get('https://www.canddi.com', (resp) =>
   resp.on('data', (chunk) => {data += chunk;});
 
   // The whole response has been received. Print out the result.
-  resp.on('end', ()    => {/*console.log("got the data")*/  FindThings(data);});
+  resp.on('end', ()    => {FindThings(data);});
 
 }).on("error", (err) => {console.log("Error: " + err.message);});
 
@@ -46,18 +45,85 @@ function FindThings(input)
     // initialise the string - IMPORTANT
     knwlInstance.init(input);
 
-    // Grab the emails in the string
-    var emails = knwlInstance.get('emails');
+    // grab the emails
+    GetEmails(input);
+    GetPhones(input);
+    GetLocs(input);
+    
+}
 
-    // THIS PRINTS OUT IN THE FORMAT (plugin properties, preiview, instances found)
-    if(emails[0])
+function GetPhones(input)
+{
+    // Grab the phones in the string
+    var phones = knwlInstance.get('phones');
+    
+    //parse the data we care about into a readable format
+    for (i=0; i < phones.length; i++)
     {
-        console.log(emails[0].found);
+        phoneList.push(phones[i]);
+    }
+
+    // print the phone numbers - if tehre are 1 or more
+    // THIS PRINTS OUT IN THE FORMAT (plugin properties, preiview, instances found)
+    if(phoneList[0])
+    {
+        console.log("\n Phone Numbers as follows")
+        console.log(phoneList);
     }
     else
     {
-        console.log("nothing in there")
+        console.log("\n no phone numbers found")
+    }
+}
+
+function GetEmails(input)
+{
+    // Grab the emails in the string
+    var emails = knwlInstance.get('emails');
+    
+    //parse the data we care about into a readable format
+    for (i=0; i < emails.length; i++)
+    {
+        emailList.push(emails[i].address);
     }
 
+    // print the emails
+    // THIS PRINTS OUT IN THE FORMAT (plugin properties, preiview, instances found)
+    if(emailList[0])
+    {
+        console.log("\n Email Addresses as follows")
+        console.log(emailList);
+    }
+    else
+    {
+        console.log("\n no emails found")
+    }
+}
 
+function GetLocs(input)
+{
+    // Grab the Locations in the string
+    var places = knwlInstance.get('places');
+    
+    //parse the data we care about into a readable format
+    for (i=0; i < places.length; i++)
+    {
+        // create a string from the info given to us
+        // will be something like places[i].Streetname + places[i].city + places[i].postcode
+        var address = '';
+      
+        locList.push(address);
+    }
+
+    // print the addresses
+    // THIS PRINTS OUT IN THE FORMAT (plugin properties, preiview, instances found)
+    if(locList[0])
+    {
+        console.log("\n location Addresses as follows")
+        console.log(locList);
+    }
+    else
+    {
+        console.log("\n no addresses found")
+    }
 }

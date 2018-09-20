8752e61d6a772a429020a60142fe656a6851c7e9
thunderrealm <38511635+thunderrealm@users.noreply.github.com>
2018-09-20 13:06:07 +0100

    Load modules
    an attempt at loading the modules; require, cherio and knwl.
    
    attempting to make sure that i ahve the correct authors, as the local Github app is logged in as my Business account.
    
    Co-Authored-By: NeonDrive <neondrive@users.noreply.github.com>

--- Folder/TestApp.js
+++ Folder/TestApp.js
@@ -1,2 +1,13 @@
-var msg = "Much String";
+// load require? is this required?
+var requireLoad = require('require/server').listen(1234);
+
+// load cherio module
+var cherio = require('cherio');
+
+// load knwl module
+var knwl = require('knwl.js');
+var knwlInstance = new knwl('English');
+
+var msg = "The modules are loaded";
 console.log(msg);
+
